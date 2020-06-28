e65789fc78eb8975e36fa3be059c2eb3aeadcafd
Faridawati <61900051+faridawati@users.noreply.github.com>
2020-06-29 01:58:02 +0800

    Add files via upload

---
+++ MouseMotionListener_1.java
@@ -0,0 +1,13 @@
+package no3;
+
+/*
+Nama      : arvinayudithia_13020180093
+Kelas     : B2
+hari/tgl  : Minggu, 15 Mei 2020
+ */
+
+public class MouseMotionListener_1 {
+    public static void main (String[] args) {
+        new MyFrame();
+    }
+}
